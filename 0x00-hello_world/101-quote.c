bd98a014b9ad243ea23a31a70643e33fa4eb3fe2
hareesdevkano <hareesabdulakdir@gmail.com>
2022-10-13 14:16:50 -0700

    task 4-8 level C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Print a text
+ *
+ * Return: Always 1 (succes)
+ */
+int main(void)
+{
+	write(2,"an the that piece of art is useful\" -Dora Korpar,2015-1019\n",59);
+	Return (1);
+}
+
