7bd12e3fede39b8b7bd6e86ffa021bf943de08e3
hareesdevkano <114575410+hareesdevkano@users.noreply.github.com>
2022-10-13 18:58:15 -0300

    update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Print a text
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2,"an the that piece of art is useful\" -Dora Korpar,2015-1019\n",59);
+	write(2,"and that piece of art is useful\" -Dora Korpar,2015-1019\n",59);
 	Return (1);
 }
 
