c974b40ce5877628820eacbc01de0ca7727f490b
hareesdevkano <hareesabdulakdir@gmail.com>
2022-10-14 17:11:55 -0700

    my all variables if else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	for (n = 0 ; n < 16 ; n++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else {
+			putchar(87 + n);
+
+		}
+	}
+	putchar('\n');
+		return (0);
+
+}
