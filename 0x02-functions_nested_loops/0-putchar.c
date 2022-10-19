7912746dc3f878d9dafdfc372e4b31c2af03e0ff
hareesdevkano <114575410+hareesdevkano@users.noreply.github.com>
2022-10-19 06:57:23 -0300

    update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
-#include "_putchar"
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- *
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char *sh = "_putchar";
+int n;
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	
-		_putchar('\n');
-		
-		return (0);
-
-	}
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%i is positive\n", n);
+else if (n == 0)
+printf("%i is zero\n", n);
+else
+printf("%i is negative\n", n);
+return (0);
+}
