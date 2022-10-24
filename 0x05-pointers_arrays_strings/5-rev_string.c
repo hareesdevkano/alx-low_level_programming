b01726e363917fc81e97d8de0242227e37804208
hareesdevkano <114575410+hareesdevkano@users.noreply.github.com>
2022-10-24 20:00:44 -0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: The string to be modified
- *
- * Return: void
- */
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
 {
 	int i, c, k;
@@ -17,14 +17,13 @@ void rev_string(char *s)
 	{
 		c++;
 	}
-	
+
 	for (k = 1; k < c; k++)
 	{
 		a++;
 	}
 
 	for (i = 0; i < (c / 2); i++)
-
 	{
 		aux = s[i];
 		s[i] = *a;
