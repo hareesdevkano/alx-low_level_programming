12ff84de056fc7300325b8ca6d6a73e46c8c337b
hareesdevkano <114575410+hareesdevkano@users.noreply.github.com>
2022-10-31 20:29:53 -0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strstr - locate a substring
- * @haystack: the string to search
- * @needle: the string to find
- *
- * Return: char value
- */
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
 char *_strstr(char *haystack, char *needle)
 {
 	int a = 0, b = 0;
@@ -30,8 +30,6 @@ char *_strstr(char *haystack, char *needle)
 
 		a++;
 	}
-	
+
 	return ('\0');
 }
-
-
